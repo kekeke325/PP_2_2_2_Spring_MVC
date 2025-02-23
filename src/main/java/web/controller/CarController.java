e57c19322a4345969f698f4cc032efc9d06c7162
kekeke <muscarin69@gmail.com>
2025-02-23 22:07:50 +0300

    Solution

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,17 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+@Controller
+public class CarController {
+
+    @GetMapping("/cars")
+    public String printCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        model.addAttribute("list", CarService.getCars(count));
+        return "cars";
+    }
+}
